8ef5f63e89fd77aea9370435be9f8a700528c81b
Jon Allured <jon.allured@gmail.com>
2022-11-15 22:00:57 -0600

    Initial Commit

---
+++ Artsy Viewer/Artsy_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Artsy_ViewerApp.swift
+//  Artsy Viewer
+//
+//  Created by Jonathan Allured on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Artsy_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
